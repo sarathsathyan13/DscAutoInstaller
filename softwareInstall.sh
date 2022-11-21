ec8fc94a22c51339b2900aff8834dc89231a8f06
SARATH SATHYAN <84668724+sarathsathyan13@users.noreply.github.com>
2022-11-21 11:38:03 +0530

    deleted softwareinstall.sh file

--- softwareInstall.sh
+++
@@ -1,9 +0,0 @@
-#Author : Sarath P S
-#Digital Signature Software Installation Process Autumation
-
-#! /bin/bash
-curl -o proxkey_ubantu.deb https://www.support.cryptoplanet.in/DownloadFiles/proxkey_ubantu.deb
-echo "proxkey_ubuntu token tool download completed"
-
-curl -o NICDSign.deb https://bims.treasury.kerala.gov.in/treasury/dsc/NICDSign.deb
-echo "NICDsign download completed"
