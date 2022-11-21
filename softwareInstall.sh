2e6a4bd5202dc63fa54fa3e72e4c97298c15bc8c
SARATH SATHYAN <84668724+sarathsathyan13@users.noreply.github.com>
2022-11-21 11:29:50 +0530

    updated

--- softwareInstall.sh
+++ softwareInstall.sh
@@ -2,11 +2,8 @@
 #Digital Signature Software Installation Process Autumation
 
 #! /bin/bash
-echo "java download completed"
-sleep 5
 curl -o proxkey_ubantu.deb https://www.support.cryptoplanet.in/DownloadFiles/proxkey_ubantu.deb
 echo "proxkey_ubuntu token tool download completed"
-sleep 5
+
 curl -o NICDSign.deb https://bims.treasury.kerala.gov.in/treasury/dsc/NICDSign.deb
 echo "NICDsign download completed"
-sleep 5
