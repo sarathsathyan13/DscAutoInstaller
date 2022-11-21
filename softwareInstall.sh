dfdf93256472701263ce846359157fb37ae0ebe7
SARATH SATHYAN <84668724+sarathsathyan13@users.noreply.github.com>
2022-11-21 11:00:56 +0530

    file updated

--- softwareInstall.sh
+++ softwareInstall.sh
@@ -3,13 +3,12 @@
 
 #!/bin/bash
 echo "Your current working directory $PWD "
-mkdir $HOME/DscSoftware
 sudo apt install default-jre -y
 echo "java download completed"
 sleep 5
-curl -o $HOME/DscSoftware/proxkey_ubantu.deb https://www.support.cryptoplanet.in/DownloadFiles/proxkey_ubantu.deb
+curl -o proxkey_ubantu.deb https://www.support.cryptoplanet.in/DownloadFiles/proxkey_ubantu.deb
 echo "proxkey_ubuntu token tool download completed"
 sleep 5
-curl -o $HOME/DscSoftware/NICDSign.deb https://bims.treasury.kerala.gov.in/treasury/dsc/NICDSign.deb
+curl -o NICDSign.deb https://bims.treasury.kerala.gov.in/treasury/dsc/NICDSign.deb
 echo "NICDsign download completed"
 sleep 5
