62a20f2fbf4fc384cefe6cb5dc153d79dd9b6593
SARATH SATHYAN <84668724+sarathsathyan13@users.noreply.github.com>
2022-11-21 11:29:17 +0530

    updated

--- softwareInstall.sh
+++ softwareInstall.sh
@@ -1,9 +1,7 @@
-# Author : Sarath P S
-# Digital Signature Software Installation Process Autumation
+#Author : Sarath P S
+#Digital Signature Software Installation Process Autumation
 
-#!/bin/bash
-echo "Your current working directory $PWD "
-sudo apt install default-jre -y
+#! /bin/bash
 echo "java download completed"
 sleep 5
 curl -o proxkey_ubantu.deb https://www.support.cryptoplanet.in/DownloadFiles/proxkey_ubantu.deb
